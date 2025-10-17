4345b4a8ccb2d47bd71cef1e43d6b70b2af1051b
YammyD <rjymt78@gmail.com>
2025-10-17 16:09:01 +0900

    add cargo run

--- shortcuts/boot.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -bios third_party/ovmf/RELEASEX64_OVMF.fd -drive format=raw,file=fat:rw:mnt
--- shortcuts/build_and_cp.sh
+++
@@ -1,7 +0,0 @@
-cargo build --target x86_64-unknown-uefi
-
-echo "build ok"
-
-cp target/x86_64-unknown-uefi/debug/wasabi.efi mnt/EFI/BOOT/BOOTX64.EFI
-
-echo "cp ok"
